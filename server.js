ebc50b7735bb1c1bc5ec07ca7ddbcff1f1a0babf
ahmedelbakly <ahmedelbakly258@gmail.com>
2023-01-13 23:31:20 +0200

    edit-server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 //Start import routes
 const groupRouts = require("./Routes/group.route");
