06eb8c33a3737d8a6e01ed4c0a3c1ff38ead0925
ahmedelbakly <ahmedelbakly258@gmail.com>
2023-01-19 14:40:06 +0200

    auth2

--- server.js
+++ server.js
@@ -32,10 +32,10 @@ main().catch((err) => console.log(err));
 app.use(
   cors({
     origin: "*",
-    //   "methods": "GET,PUT,POST,DELETE",
-    //   "preflightContinue": false,
-    //   "optionsSuccessStatus": 204,
-    //  "allowedHeaders":"Authorization"
+      "methods": "GET,PUT,POST,DELETE",
+      "preflightContinue": false,
+      "optionsSuccessStatus": 204,
+     "allowedHeaders":"Authorization"
   })
 );
 
