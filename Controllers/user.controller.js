33ddc5a6fd8c3a67212d5ec705b21ffe27df10b7
ahmedelbakly <ahmedelbakly258@gmail.com>
2023-02-16 19:04:56 +0200

    mail2

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -45,7 +45,7 @@ exports.signupUser = async (req, res, next) => {
         // const link = ` http://localhost:5000/api/user/active/${activeToken}`
         const link = ` https://center-app.vercel.app//api/user/active/${activeToken}`;
        await nodeMiller.sendMailToUser(email, "activation Email", link);
-        res.status(201).json(newUser);
+      return  res.status(201).json(newUser);
       }
     }
   } catch (error) {
